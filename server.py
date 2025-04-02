5a36ec2d7cc320a375bb7b884a5f0581df000b1e
kaPHANTOM <liteis6524@gmail.com>
2025-04-02 17:55:24 +0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, jsonify
+from flask_socketio import SocketIO, emit
+
+app = Flask(name)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+users = {}
+
+@app.route('/login')
+def login():
+    temp_id = os.urandom(8).hex()
+    search_code = os.urandom(4).hex()
+    users[temp_id] = {'online': True, 'search_code': search_code}
+    return jsonify({'temp_id': temp_id, 'search_code': search_code})
+
+@app.route('/status', methods=['POST'])
+def status():
+    return jsonify({'online': True})
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('message')
+def handle_message(data):
+    emit('message', data, broadcast=True)
+
+if name == 'main':
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
